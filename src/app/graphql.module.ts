1e1bb6557d5e32dc05e23c96cd9506dead375595
Matheus Braz <theus.braz18@gmail.com>
2023-11-17 03:16:02 -0300

    env config

--- src/app/graphql.module.ts
+++ src/app/graphql.module.ts
@@ -2,7 +2,7 @@ import { APOLLO_OPTIONS, ApolloModule } from 'apollo-angular';
 import { HttpLink } from 'apollo-angular/http';
 import { NgModule } from '@angular/core';
 import { ApolloClientOptions, InMemoryCache } from '@apollo/client/core';
-import { environment } from 'src/environments/environments';
+import { environment } from './../environments/environment';
 
 export function createApollo(httpLink: HttpLink): ApolloClientOptions<any> {
   return {
