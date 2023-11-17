ecd91a846967d85ce25d36413329cfa3fdbee5d7
Matheus Braz <theus.braz18@gmail.com>
2023-11-17 03:18:15 -0300

    prod env

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  ApiUrl: 'https://dropmail.me/api/graphql/bWF0aGV1cy10ZXN0ZQ',
+  ApiUrl:
+    'https://cors-anywhere.herokuapp.com/https://dropmail.me/api/graphql/bWF0aGV1cy10ZXN0ZQ',
 };
