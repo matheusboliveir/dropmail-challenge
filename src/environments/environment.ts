0b0bcd2f79b2f06dd75acb94fc0bc64c029d7711
matheusboliveir <theus.braz18@gmail.com>
2023-11-17 12:01:55 -0300

    prod env

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   ApiUrl:
-    'https://cors-anywhere.herokuapp.com/https://dropmail.me/api/graphql/bWF0aGV1cy10ZXN0ZQ',
+    'https://cors-proxy-server-nu.vercel.app/https://dropmail.me/api/graphql/bWF0aGV1cy10ZXN0ZQ',
 };
